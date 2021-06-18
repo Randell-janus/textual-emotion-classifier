ee9ed1f1a275e52b3589d4195ccc5bae79e67245
Hiich <raaanjnpmcn@gmail.com>
2021-06-18 12:04:59 +0800

    replace url

--- pages/fastapi.js
+++ pages/fastapi.js
@@ -9,7 +9,7 @@ export default function Home() {
   async function getSentiment() {
     try {
       const response = await fetch(
-        `http://127.0.0.1:8000/goemotions?text=${text}`,
+        `https://serverless-py.vercel.app/goemotions?text=${text}`,
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
@@ -35,7 +35,7 @@ export default function Home() {
   };
 
   const tapServer = () => {
-    return fetch("http://127.0.0.1:8000/")
+    return fetch("https://serverless-py.vercel.app/")
       .then((resp) => resp.json())
       .then((data) => {
         console.log(data);
