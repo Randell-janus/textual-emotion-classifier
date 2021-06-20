f8820fced18ce1bcb574ed8318766b95a817c641
Hiich <raaanjnpmcn@gmail.com>
2021-06-21 02:35:26 +0800

    initial

---
+++ pages/api/classify.js
@@ -0,0 +1,17 @@
+const fetch = require("node-fetch");
+
+async function handler(req, res) {
+  const response = await fetch(
+    "https://api-inference.huggingface.co/models/joeddav/distilbert-base-uncased-go-emotions-student",
+    {
+      headers: { Authorization: `Bearer ${process.env.HF_KEY}` },
+      method: "POST",
+      body: JSON.stringify(req.body.sentence),
+    }
+  );
+  const data = await response.json();
+//   console.log(data);
+  return res.json(data);
+}
+
+export default handler;
